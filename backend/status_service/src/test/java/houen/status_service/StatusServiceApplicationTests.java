9bfa896f67d7ed46f17d6c0296b7e6f4ff008e4e
Mateusz Hołenko <mateusz.holenko@gmail.com>
2025-03-25 19:48:17 +0100

    backend: Add status service stub

---
+++ backend/status_service/src/test/java/houen/status_service/StatusServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package houen.status_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatusServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
