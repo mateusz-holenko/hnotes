2b69b22ad7951d263886b01ef54f6fb87705755d
Mateusz Hołenko <mateusz.holenko@gmail.com>
2025-03-29 15:20:19 +0100

    nix: Set env for notes service

--- backend/notes_service/shell.nix
+++ backend/notes_service/shell.nix
@@ -4,6 +4,13 @@ let
 in
 
 pkgs.mkShellNoCC {
+  DB_HOST="localhost";
+  DB_PORT=3306;
+  DB_USERNAME="houen";
+  DB_PASSWORD="houen";
+  VERIFICATION_SERVICE_HOST="localhost";
+  VERIFICATION_SERVICE_PORT=5000;
+
   packages = with pkgs; [
     maven
     jdk17
