44d212ec90f4082677f5ff8b7adbd6ded19e3a02
Mateusz Hołenko <mateusz.holenko@gmail.com>
2025-03-31 13:48:29 +0200

    backend: verification: Add errors

--- backend/verification_service/veri.py
+++ backend/verification_service/veri.py
@@ -1,6 +1,17 @@
-from flask import Flask, request
+from flask import Flask, request, Response
+from werkzeug.exceptions import InternalServerError
+
 
 app = Flask(__name__)
+ctr = 0
+
+
+def maybe_error():
+    global ctr
+    ctr += 1
+    if ctr % 5 == 0:
+        raise InternalServerError('Encountered an internal error, pls try again later')
+
 
 @app.route('/')
 def health_check():
@@ -9,6 +20,8 @@ def health_check():
 
 @app.route('/verificator', methods=['POST'])
 def verify_input():
+    maybe_error()
+
     input = str(request.data)
     accepted = 'banana' not in input
     result = {
@@ -17,3 +30,8 @@ def verify_input():
     }
     app.logger.info('Input is: ' + input + "; result is: " + str(result))
     return result
+
+
+@app.route('/error')
+def get_error():
+    return Response('there seems to be an error', 500)
