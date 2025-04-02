784e26eb1c8459b928a45c9e72b7e9bfc63e937b
Mateusz Hołenko <mateusz.holenko@gmail.com>
2025-04-02 20:31:03 +0200

    backend: notes: Remove unnecessary impors

--- backend/notes_service/src/main/java/houen/hnotes/NotesRestController.java
+++ backend/notes_service/src/main/java/houen/hnotes/NotesRestController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.server.ResponseStatusException;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.slf4j.LoggerFactory;
