2a26d8e3b78c5905c33cabb2189d9489c673e298
Mateusz Hołenko <mateusz.holenko@gmail.com>
2025-04-02 16:50:59 +0200

    backend: notes: Extract Artemis service

--- backend/notes_service/src/main/java/houen/hnotes/NotesRestController.java
+++ backend/notes_service/src/main/java/houen/hnotes/NotesRestController.java
@@ -25,46 +25,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
-
-import jakarta.jms.Message;
-import jakarta.jms.TextMessage;
-import jakarta.jms.Session;
-import jakarta.jms.JMSException;
-
-@Service
-class ArtemisService {
-
-  private final JmsTemplate t;
-
-  @Autowired
-  public ArtemisService(JmsTemplate jmsTemplate) {
-    this.t = jmsTemplate;
-  }
-
-  public void send(String text) {
-    t.send("testdest", new MessageCreator() {
-      public Message createMessage(Session session) throws JMSException {
-        return session.createTextMessage(text);
-      }
-    });
-  }
-
-  public String receive() {
-    try {
-    var msg = (TextMessage) t.receive("testdest");
-    return msg.getText();
-    } catch (Exception e) {
-      return "[EXCEPTION]";
-    }
-  }
-}
-
 @RestController
 @CrossOrigin(origins = "*")
 public class NotesRestController {
-  
     @Autowired
     private ArtemisService artemisService;
     
