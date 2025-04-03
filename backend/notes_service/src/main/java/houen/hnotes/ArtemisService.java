56bd13781f2a4c342dc456b2cf3ce2cc3f39cb78
Mateusz Hołenko <mateusz.holenko@gmail.com>
2025-04-03 12:00:18 +0200

    backend: notes: Improve Artemis support

--- backend/notes_service/src/main/java/houen/hnotes/ArtemisService.java
+++ backend/notes_service/src/main/java/houen/hnotes/ArtemisService.java
@@ -3,6 +3,9 @@ package houen.hnotes;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.slf4j.LoggerFactory;
+
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
 
@@ -10,6 +13,11 @@ import jakarta.jms.Message;
 import jakarta.jms.TextMessage;
 import jakarta.jms.Session;
 import jakarta.jms.JMSException;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.json.JSONObject;
 
 @Service
 public class ArtemisService {
@@ -22,7 +30,7 @@ public class ArtemisService {
   }
 
   public void send(String text) {
-    t.send("testdest", new MessageCreator() {
+    t.send("verification.queue", new MessageCreator() {
       public Message createMessage(Session session) throws JMSException {
         return session.createTextMessage(text);
       }
@@ -37,5 +45,18 @@ public class ArtemisService {
       return "[EXCEPTION]";
     }
   }
+
+	@JmsListener(destination = "verification-result.queue")
+	public void processMessage(String content) {
+	  var logger = LoggerFactory.getLogger(NotesRestController.class);
+	  
+	  // TODO: this should be done by some generic STOMP handler
+	  var str = Stream.of(content.split(","))
+	    .map(x -> (new Character((char)Integer.parseInt(x)).toString()))
+	    .collect(Collectors.joining());
+	  var json = new JSONObject(str);
+	  
+	  logger.error("received!!! " + json);
+	}
 }
 
