84db61dcf93619ba9516d373c00d499467928842
Mateusz Hołenko <mateusz.holenko@gmail.com>
2025-04-16 18:00:56 +0200

    backend: notes: Remove unnecessary imports

--- backend/notes_service/src/test/java/houen/hnotes/NotesStoreTests.java
+++ backend/notes_service/src/test/java/houen/hnotes/NotesStoreTests.java
@@ -2,25 +2,11 @@ package houen.hnotes;
 
 import java.util.ArrayList;
 
-import org.apache.activemq.artemis.jms.client.ActiveMQTextMessage;
-import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Timeout;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 
 @SpringBootTest()
 public class NotesStoreTests {
