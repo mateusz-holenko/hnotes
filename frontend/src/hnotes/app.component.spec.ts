8fd195ff53b2daa85002172a81a3f717393f6891
Mateusz Hołenko <mateusz.holenko@gmail.com>
2025-04-23 15:37:27 +0200

    frontend: tests: Fix App component creation test

--- frontend/src/hnotes/app.component.spec.ts
+++ frontend/src/hnotes/app.component.spec.ts
@@ -1,14 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     }).compileComponents();
   });
 
   it('should create the app', () => {
+    TestBed.inject(HttpTestingController);
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
