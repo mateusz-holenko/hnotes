ebbe39bc6f0e0ceb96ce61e9a960d216b70d9c70
Mateusz Hołenko <mateusz.holenko@gmail.com>
2025-04-24 14:37:59 +0200

    frontend: Add search bar

--- frontend/src/hnotes/header.component.ts
+++ frontend/src/hnotes/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginStatusComponent } from './login-status.component';
 import { AppService } from './app-service';
+import { NotesService } from './notes.service';
 
 @Component({
   selector: 'header-component',
@@ -18,5 +19,9 @@ export class HeaderComponent {
     this.appService.requestRefresh();
   }
 
-  constructor(private router: Router, private appService: AppService) {}
+  onSearchChange(pattern: any) {
+    this.notesService.setFilter(pattern.value);
+  }
+
+  constructor(private router: Router, private appService: AppService, private notesService: NotesService) {}
 }
