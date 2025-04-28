44090306b84bda5df84440649423af395ade4e35
Mateusz Hołenko <mateusz.holenko@gmail.com>
2025-04-28 11:55:33 +0200

    backend: notes: Remove unused imports

--- backend/notes_service/src/main/java/houen/hnotes/NotesRestController.java
+++ backend/notes_service/src/main/java/houen/hnotes/NotesRestController.java
@@ -1,7 +1,5 @@
 package houen.hnotes;
 
-import java.util.ArrayList;
-import java.time.Instant;
 import java.security.Principal;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,15 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import org.springframework.web.server.ResponseStatusException;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.slf4j.LoggerFactory;
-
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 
 @RestController
 @CrossOrigin(origins = "*")
