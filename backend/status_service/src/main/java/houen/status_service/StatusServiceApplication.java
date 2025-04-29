ddc6b3de772201700e68e2b96e979c7f5ccd9caf
Mateusz Hołenko <mateusz.holenko@gmail.com>
2025-04-29 13:42:43 +0200

    backend: status: Configure jms as topic

--- backend/status_service/src/main/java/houen/status_service/StatusServiceApplication.java
+++ backend/status_service/src/main/java/houen/status_service/StatusServiceApplication.java
@@ -2,12 +2,24 @@ package houen.status_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+
+import jakarta.jms.ConnectionFactory;
 
 @SpringBootApplication
 public class StatusServiceApplication {
+  @Bean
+  public JmsListenerContainerFactory<?> jmsListenerFactory(ConnectionFactory jmsConnectionFactory, DefaultJmsListenerContainerFactoryConfigurer configurer) {
+    DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+    configurer.configure(factory, jmsConnectionFactory);
+    factory.setPubSubDomain(true);
+    return factory;
+  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(StatusServiceApplication.class, args);
 	}
-
 }
