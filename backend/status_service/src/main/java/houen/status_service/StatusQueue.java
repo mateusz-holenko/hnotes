b607794f6bfe08de30317d0cdb9b58bb4d41e874
Mateusz Hołenko <mateusz.holenko@gmail.com>
2025-04-30 13:51:47 +0200

    backend: status: Send status updates over WS

--- backend/status_service/src/main/java/houen/status_service/StatusQueue.java
+++ backend/status_service/src/main/java/houen/status_service/StatusQueue.java
@@ -2,8 +2,11 @@ package houen.status_service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class StatusQueue {
@@ -11,8 +14,19 @@ public class StatusQueue {
   private static final Logger Logger = LoggerFactory.getLogger(StatusQueue.class);
   private static final String StatusUpdateTopic = "status.topic";
 
+  private static final ObjectMapper mapper = new ObjectMapper();
+
+  @Autowired
+  private StompController stompController;
+
   @JmsListener(destination = StatusQueue.StatusUpdateTopic)
   public void processIncomingMessage(String content) {
-    Logger.info("Handling incoming message: >>{}<<", content);
+    Logger.debug("Handling incoming message: >>{}<<", content);
+    try {
+      var status = (StatusUpdate)mapper.readValue(content, StatusUpdate.class);
+      stompController.sendUpdate(status);
+    } catch(Exception e) {
+      Logger.error("Error while parsing message: {}", e.getMessage());
+    }
   }
 }
