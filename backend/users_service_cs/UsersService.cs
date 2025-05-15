fcffaac09de374c2272395acca490ddbd8703e11
Mateusz Hołenko <mateusz.holenko@gmail.com>
2025-05-15 13:40:07 +0200

    backend: users_cs: Fix formatting

--- backend/users_service_cs/UsersService.cs
+++ backend/users_service_cs/UsersService.cs
@@ -6,71 +6,69 @@ using Microsoft.IdentityModel.Tokens;
 
 public static class UsersService
 {
-  public class User
-  {
-    public User(int id, string username)
+    public class User
     {
-     Id = id;
-     Username = username;
+        public User(int id, string username)
+        {
+            Id = id;
+            Username = username;
+        }
+
+        public int Id { get; set; }
+        public string Username { get; set; }
     }
 
-    public int Id { get; set; }
-    public string Username { get; set; }
-  }
-
-  private class UserCredentials
-  {
-    public string? username { get; set; }
-    public string? password { get; set; }
-  }
-
-  private static List<User> users = new List<User>() {
-    new User(100, "admin"),
-    new User(101, "User1"),
-    new User(800, "Guest")
-  };
-
-  private class UsersDbContext : DbContext
-  {
-	public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options) {}
-  }
-
-  public static void Main(string[] args)
-  {
-    var builder = WebApplication.CreateBuilder(args);
-    builder.Services.AddControllers();
-
-    var dbConnectionString = "server=localhost;user=houen;password=houen;database=usersdatabase";
-    builder.Services.AddDbContext<UsersDbContext>(options => options
-      .UseMySql(dbConnectionString, new MariaDbServerVersion("11.7.2")));
-
-    var app = builder.Build();
-
-    app.MapPost("/users/login", (UserCredentials credentials) =>
+    private class UserCredentials
     {
-     var user = users.FirstOrDefault(x => x.Username == credentials.username);
-     if(user == null) {
-      return Results.BadRequest($"User '{credentials.username}' not found or provided bad credentials");
-     }
+        public string? username { get; set; }
+        public string? password { get; set; }
+    }
 
-     var currentTimestamp = DateTime.UtcNow;
-     var jwtToken = new JwtSecurityToken(
-      notBefore: currentTimestamp,
-      expires: currentTimestamp.AddHours(1),
-      claims: new List<Claim> { new Claim(ClaimTypes.Name, user.Id.ToString()) },
-      signingCredentials: new SigningCredentials(
-       new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mysecretkeythatislongenoughforthehashingalgorithmmysecretkeythan")),
-       SecurityAlgorithms.HmacSha512Signature
-      )
-     );
+    private static List<User> users = new List<User>() {
+        new User(100, "admin"),
+        new User(101, "User1"),
+        new User(800, "Guest")
+    };
 
-     return Results.Ok(new Dictionary<string, string> {
-      { "username", user.Username },
-      { "jwt", new JwtSecurityTokenHandler().WriteToken(jwtToken) }
-     });
-    });
+    private class UsersDbContext : DbContext
+    {
+        public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options) {}
+    }
 
-    app.MapControllers();
-    app.Run();
-  }
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+
+        var dbConnectionString = "server=localhost;user=houen;password=houen;database=usersdatabase";
+        builder.Services.AddDbContext<UsersDbContext>(options => options
+            .UseMySql(dbConnectionString, new MariaDbServerVersion("11.7.2")));
+
+        var app = builder.Build();
+
+        app.MapPost("/users/login", (UserCredentials credentials) =>
+        {
+           var user = users.FirstOrDefault(x => x.Username == credentials.username);
+           if(user == null) {
+              return Results.BadRequest($"User '{credentials.username}' not found or provided bad credentials");
+           }
+
+           var currentTimestamp = DateTime.UtcNow;
+           var jwtToken = new JwtSecurityToken(
+               notBefore: currentTimestamp,
+               expires: currentTimestamp.AddHours(1),
+               claims: new List<Claim> { new Claim(ClaimTypes.Name, user.Id.ToString()) },
+               signingCredentials: new SigningCredentials(
+                   new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mysecretkeythatislongenoughforthehashingalgorithmmysecretkeythan")),
+                   SecurityAlgorithms.HmacSha512Signature));
+
+           return Results.Ok(new Dictionary<string, string> {
+               { "username", user.Username },
+               { "jwt", new JwtSecurityTokenHandler().WriteToken(jwtToken) }
+           });
+        });
+
+        app.MapControllers();
+        app.Run();
+    }
 }
